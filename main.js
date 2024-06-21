968736aa1a6be54d2ff0ec43e7a50d40d3e505bd
kamillydcc <166824906+kamillydcc@users.noreply.github.com>
2024-06-21 09:40:49 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo")
 const tempo = document.querySelectorAll(".contador")
-//console.log(botoes);
-for(let i = 0; 1 < botoes.length ; i++){
+tempo[0].textContent = "hello,tudo bem?"
+const tempoObjetivo1 = new Date(2024,7,13,20);
+const tempoObjetivo2 = new Date(05);
+const tempoObjetivo3 = new Date(2006);
+const tempoObjetivo4 = new Date(10);
+const tempoObjetivo5 = new Date(09);
+const agora = new Date();
+tempo[0].textContent = tempoObjetivo1-agora
+tempo[1].textContent = tempoObjetivo1
+let segundos;
+let minutos;
+let horas;
+let dias;
+segundos= (tempoObjetivo1-agora)/1000
+minutos= segundos/60
+horas= minutos/60
+dias= horas/24
+tempo[0].textContent = `Falta ${dias} dias, ${horas}horas, ${minutos} minutos, ${segundos} segundos`
+console.log(botoes);
+for(let i = 0; i < botoes.length ; i++){
     botoes[i].onclick = function() {
         for (j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
@@ -12,5 +30,3 @@ for(let i = 0; 1 < botoes.length ; i++){
         conteudo[i].classList.add("ativo")
     }
 }
-const tempo =querySelectorAll(".contador")
-tempo
